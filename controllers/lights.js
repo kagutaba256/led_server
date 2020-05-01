bb877087281797b535020b4822188af32867cc71
Sam Little <kagutaba256@gmail.com>
2020-05-01 12:47:07 -0700

    attempt to add multiple lights with put

--- controllers/lights.js
+++ controllers/lights.js
@@ -18,25 +18,29 @@ exports.getLights = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: data });
 });
 
-// @desc    Set status of specific light
-// @route   PUT /api/v1/lights/:color/:status
+// @desc    Set status of specific light or lights
+// @route   PUT /api/v1/lights/
 // @access  Private
 exports.setLight = asyncHandler(async (req, res, next) => {
-  const { status, color } = req.params;
-  if (!board.lights[color]) {
-    return next(
-      new ErrorResponse(
-        `Could not access light with color ${req.params.color}`,
-        404
-      )
-    );
-  }
-  // TODO real error handling here..
-  // if (status.isNaN() || (status !== 0 && status !== 1)) {
-  //   return next(
-  //     new ErrorResponse(`Malformed request. Status must be 0 or 1`, 404)
-  //   );
-  // }
-  board.lights[color].writeSync(status);
-  res.status(200).json({ success: true, data: board.lights[color].readSync() });
+  data = {};
+  req.body.forEach((light) => {
+    const { color, status } = light;
+    if (!board.lights[color]) {
+      return next(
+        new ErrorResponse(
+          `Could not access light with color ${req.params.color}`,
+          404
+        )
+      );
+    }
+    // TODO real error handling here..
+    // if (status.isNaN() || (status !== 0 && status !== 1)) {
+    //   return next(
+    //     new ErrorResponse(`Malformed request. Status must be 0 or 1`, 404)
+    //   );
+    // }
+    board.lights[color].writeSync(status);
+    data[color] = board.lights[color].readSync();
+  });
+  res.status(200).json({ success: true, data: data });
 });
